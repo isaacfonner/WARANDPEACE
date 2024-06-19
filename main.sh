328f16f2b8dfa294a203f3c2423a034774a182b7
isaacfonner <61657908+isaacfonner@users.noreply.github.com>
2024-06-19 20:09:41 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo starting...
+cd bungee
+java -Xmx1024M -Xms1024M -jar bungee.jar
+
+
+
+#  
+
+#Ilost # Not in the mood because I cant use discord.
+# what was your project called in github we might be able to open it back up :D
